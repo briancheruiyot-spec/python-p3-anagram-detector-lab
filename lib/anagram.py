3f711277601d39423fb57d0fa3f7c2c039ece515
briancheruiyot-spec <briancheruiyot6@gmail.com>
2025-05-12 12:18:07 +0300

    updated files

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)  # Sort the letters of the initialized word
+        matches = []
+        for candidate in candidates:
+            if sorted_word == sorted(candidate):  # Compare sorted version of candidate to sorted word
+                matches.append(candidate)
+        return matches
